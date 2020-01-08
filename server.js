2e343689da716b211313cd1fa836b58a76a24143
David Sellam <dvdsellam@gmail.com>
2020-01-08 22:06:24 +0200

    Models User created

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const connectDB = require("./config/db");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
+// connect DB
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes
-
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
